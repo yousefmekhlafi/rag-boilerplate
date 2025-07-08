773bc9cd44fabfac20deb0c08908551d61272564
yousefmekhlafi <yousefmekhlafi@gmail.com>
2025-07-08 12:36:05 +0300

    file-upload-done

---
+++ src/helpers/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# This configuration class will be responsible for loading the .env file as well as configurations for the entire backend's logic
+class Settings(BaseSettings):
+
+    APP_NAME: str
+    APP_VERSION: str
+
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
+
+
+    class Config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings() 
+
+
---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+
+    FILE_VALIDATION_SUCCEEDED = "file_validated_successfully"
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_SIZE_EXCEEDED = "file_size_exceeded"
+    FILE_UPLOAD_SUCCEEDED = "file_uploaded_successfully"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
