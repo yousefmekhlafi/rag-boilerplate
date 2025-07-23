cd8bc9a92dc71389f7e01013eccbc0ec7bdaa96a
yousefmekhlafi <yousefmekhlafi@gmail.com>
2025-07-23 10:17:53 +0300

    llm-factory-added

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -16,7 +16,23 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DATABASE: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
+
+    
+    class Config:
+        env_file = ".env"
 
 def get_settings():
     return Settings() 
--- src/main.py
+++ src/main.py
@@ -2,19 +2,30 @@ from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
-
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 app = FastAPI()
 
-@app.on_event("startup")
 async def startup_db_client():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
-@app.on_event("shutdown")
+    llm_provider_factory = LLMProviderFactory(settings)
+
+    # generation client 
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+    
+    #embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
 async def shutdown_db_client():
     app.mongo_conn.close()
 
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
