3229f0282e02406e4a686bdae7e58cbe6799294b
yousefmekhlafi <yousefmekhlafi@gmail.com>
2025-07-27 21:29:08 +0300

    vectordb-added

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -15,7 +15,23 @@ class BaseController:
             "assets",
             "files"
         )
+
+        self.database_dir = os.path.join(
+            self.base_dir,
+            "assets/database"
+        )
         
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    
+    def get_database_path(self, db_name: str):
+
+        database_path = os.path.join(
+            self.database_dir, db_name
+        )
+
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+
+        return database_path
        
--- src/helpers/config.py
+++ src/helpers/config.py
@@ -31,6 +31,11 @@ class Settings(BaseSettings):
     GENERATION_DEFAULT_TEMPERATURE: float = None
 
     
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_PATH: str 
+    VECTOR_DB_DISTANCE_METHOD: str
+
+    
     class Config:
         env_file = ".env"
 
